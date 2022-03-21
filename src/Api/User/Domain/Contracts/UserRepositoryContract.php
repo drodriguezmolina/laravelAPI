01ccc400fde3c77445842c8b108d0d31462a68cb
Diego Rodríguez <drodriguez@omatech.com>
2022-03-21 20:59:56 +0100

    Update contract

--- src/Api/User/Domain/Contracts/UserRepositoryContract.php
+++ src/Api/User/Domain/Contracts/UserRepositoryContract.php
@@ -2,14 +2,13 @@
 
 namespace Src\Api\User\Domain\Contracts;
 
-use Ramsey\Uuid\Uuid;
 use Src\Api\User\Domain\User;
-use Src\Api\User\Domain\ValueObjects\UserEmail;
 use Src\Api\User\Domain\ValueObjects\UserId;
-use Src\Api\User\Domain\ValueObjects\UserName;
 
 interface UserRepositoryContract
 {
+    public function all(): array;
+
     public function find(UserId $id): ?User;
 
     public function save(User $user): void;
