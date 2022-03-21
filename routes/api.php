67b0a18f0acd982c8468f4d96b4c4f0649a707d9
Diego Rodríguez <drodriguez@omatech.com>
2022-03-21 20:59:07 +0100

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CreateUserController;
+use App\Http\Controllers\DeleteUserController;
+use App\Http\Controllers\GetMostUsedDomainsController;
+use App\Http\Controllers\GetUserController;
+use App\Http\Controllers\UpdateUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('user/{id}', 'GetUserController');
-Route::get('user/most-used-domains', 'GetMostUsedDomainsController');
-Route::post('user', 'CreateUserController');
-Route::put('user/{id}', 'UpdateUserController');
-Route::delete('user/{id}', 'DeleteUserController');
+Route::get('user/{id}', GetUserController::class);
+Route::get('user/most-used-domains', GetMostUsedDomainsController::class);
+Route::post('user', CreateUserController::class);
+Route::put('user/{id}', UpdateUserController::class);
+Route::delete('user/{id}', DeleteUserController::class);
