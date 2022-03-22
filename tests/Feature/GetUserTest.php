94a287a9215ba598a56f6b0e62185329357eeddd
Diego Rodríguez <drodriguez@omatech.com>
2022-03-22 09:21:25 +0100

    Add tests get user

---
+++ tests/Feature/GetUserTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetUserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_created_user_is_retrieved()
+    {
+        $this->withoutExceptionHandling();
+        $this->postJson('/api/user', $this->data())
+            ->assertStatus(201);
+
+        $this->assertCount(1, User::all());
+        $user = User::first();
+
+        $this->json('GET', "/api/user/$user->id")
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'name' => 'Firstname Lastname',
+                    'email' => "firstname.lastname@gmail.com",
+                    'emailVerifiedDate' => null
+                ]
+            ]);
+    }
+
+    private function data()
+    {
+        return [
+            'name' => "Firstname Lastname",
+            'email' => "firstname.lastname@gmail.com",
+            'password' => "password_pwd"
+        ];
+    }
+}
